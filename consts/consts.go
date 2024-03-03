9c19e533498f4bebc90722deb4a0b1c270a6a766
sandeep1719 <157355485+sandeep1719@users.noreply.github.com>
2024-03-03 23:46:17 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "metaverse"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "MT"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "mt"
 )
 
 var ID ids.ID
